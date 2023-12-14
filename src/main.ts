a30f151b6853ff5beeb6ea60ac700f2683e3714e
Daniel Chyan <bitnovus@users.noreply.github.com>
2023-12-14 08:52:06 -0800

    feat: changes back to gh action
    change back to github actions instead of a CLI tool

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,29 @@
-#!/usr/bin/env node
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import * as core from "@actions/core";
 import { DateTime } from "luxon";
-import { generateReport } from "./report.js";
+import { generateReport } from "./report";
 import Heroku from "heroku-client";
 
-// Define an interface for the expected command line arguments
-interface CommandLineArgs {
-  heroku_api_token: string;
-  heroku_app_name: string;
-  report_end_date: string;
-  report_timeframe: number;
-}
+async function run(): Promise<void> {
+  const heroku_api_token = core.getInput("heroku_api_token");
+  const herokuClient = new Heroku({ token: heroku_api_token });
 
-// Parse command line arguments and assert their type
-const argv = yargs(hideBin(process.argv))
-  .usage(
-    "Usage: $0 --heroku_api_token [string] --heroku_app_name [string] --report_end_date [string] --report_timeframe [num]"
-  )
-  .option("heroku_api_token", {
-    describe: "Heroku API token",
-    type: "string",
-    demandOption: true,
-  })
-  .option("heroku_app_name", {
-    describe: "Heroku app name",
-    type: "string",
-    demandOption: true,
-  })
-  .option("report_end_date", {
-    describe: "End date for the report in ISO format",
-    type: "string",
-    default: DateTime.fromJSDate(new Date()).endOf("day").toISO(),
-  })
-  .option("report_timeframe", {
-    describe: "Timeframe for the report in months",
-    type: "number",
-    default: 1,
-  })
-  .help()
-  .alias("help", "h").argv as unknown as CommandLineArgs;
-
-async function run() {
-  const herokuClient = new Heroku({ token: argv.heroku_api_token });
+  const appName = core.getInput("heroku_app_name");
+  if (appName === undefined) {
+    throw new Error("heroku_app_name must be defined.");
+  }
   const reportFileName = "./report.html";
 
-  const reportEnd = DateTime.fromISO(argv.report_end_date);
-  const reportStart = reportEnd.minus({ months: argv.report_timeframe });
+  const reportEndInput = core.getInput("report_end_date");
+  const reportEnd = reportEndInput
+    ? DateTime.fromISO(reportEndInput)
+    : DateTime.fromJSDate(new Date()).endOf("day");
+
+  const reportTimeframe = core.getInput("report_timeframe");
+  const monthsBack = parseInt(reportTimeframe, 10);
+  const reportStart = reportEnd.minus({ months: monthsBack });
 
   const reportParams = {
-    herokuAppName: argv.heroku_app_name,
+    herokuAppName: appName,
     reportFileName,
     reportStart,
     reportEnd,
@@ -59,12 +33,16 @@ async function run() {
 
   try {
     await generateReport(reportParams, herokuClient);
-    console.log(
+    core.debug(
       `New report generated successfully. Available at ${reportFileName}`
     );
+    core.setOutput("report", reportFileName);
   } catch (error) {
-    console.error(error);
-    process.exit(1);
+    if (error instanceof Error) {
+      core.setFailed(error);
+    } else {
+      core.setFailed("An unknown error occurred");
+    }
   }
 }
 
